3b45a77a078528686bee071141e6fd583b24dc29
kimhyunhee <hyunhi7@naver.com>
2017-05-31 16:08:52 +0900

    chatt

--- Practice/src/test/main/testProgram.java
+++ Practice/src/test/main/testProgram.java
@@ -3,8 +3,12 @@ package test.main;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -15,6 +19,7 @@ import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
@@ -27,9 +32,9 @@ public class testProgram extends JFrame implements ActionListener{
 	DefaultTableModel model;
 	JButton saveBtn, deleteBtn, updateBtn;
 	JTable table;
-	JCheckBox inputCheck;
 	JComboBox inputMonth;
 	JComboBox inputDate;
+	JComboBox inputCheck;
 	
 	List<Map<String, Object>> todoList;
 	//생성자
@@ -50,6 +55,8 @@ public class testProgram extends JFrame implements ActionListener{
 		JLabel label3=new JLabel("할 일");
 		
 		//텍스트 필드 객체 생성
+		inputCheck=new JComboBox();
+		inputCheck.setModel(new DefaultComboBoxModel(new String[]{"안함","진행중","완료"}));
 		inputMonth=new JComboBox();
 		inputMonth.setModel(new DefaultComboBoxModel(new String[]{"1","2","3","4","5","6","7","8","9","10","11","12"}));
 		inputDate=new JComboBox();
@@ -62,8 +69,12 @@ public class testProgram extends JFrame implements ActionListener{
 		saveBtn=new JButton("저장");
 		deleteBtn=new JButton("삭제");
 		updateBtn=new JButton("수정");
-
+		//버튼 리스너 등록 
+		saveBtn.addActionListener(this);
+		deleteBtn.addActionListener(this);
+		updateBtn.addActionListener(this);
 		//페널에 컴포넌트 추가 하기 
+		topPanel.add(inputCheck);
 		topPanel.add(inputMonth);
 		topPanel.add(label1);
 		topPanel.add(inputDate);
@@ -79,7 +90,7 @@ public class testProgram extends JFrame implements ActionListener{
 		add(topPanel, BorderLayout.NORTH);		
 		
 		//테이블 칼럼명 
-		String[] colNames={"check", "날짜", "할 일"};
+		String[] colNames={"진행상황", "날짜", "할 일"};
 		//테이블에 연결할 모델 객체 
 		model=new DefaultTableModel(colNames, 0);
 		
@@ -101,36 +112,66 @@ public class testProgram extends JFrame implements ActionListener{
 		// 창 닫을때 프로세스 종료 되도록 
 		setDefaultCloseOperation(EXIT_ON_CLOSE);	
 		
+		//파일에 있는 정보 로딩하기
+		loadFromFile();
+		//로딩된 정보 출력하기 
+		displayMember();
+		
 	}	
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if(e.getSource() == saveBtn){
+			String Check = (String)inputCheck.getSelectedItem();
 			String Todo = inputTodo.getText();
-			String Month = inputMonth.toString();
-			String Date = inputDate.toString();
+			String Month = (String)inputMonth.getSelectedItem();
+			String Date = (String)inputDate.getSelectedItem();
 			Map<String, Object> map=new HashMap<>();
+			map.put("Check", Check);
 			map.put("ToDo", Todo);
 			map.put("Month", Month);
 			map.put("Date", Date);
 			todoList.add(map);
 			saveToFile();
+			
+			JOptionPane.showMessageDialog(this, "저장했습니다.");
+			inputTodo.setText("");
 		}else if(e.getSource() == updateBtn){
-			
+			int selectedIndex=table.getSelectedRow();
+			if(selectedIndex==-1){
+				JOptionPane.showMessageDialog(this, "수정할 row를 선택하세요");
+				return;
+			}
+			String check = (String)inputCheck.getSelectedItem();
+			//String check=(String)table.getValueAt(selectedIndex, 0);
+			String todo=(String)table.getValueAt(selectedIndex, 2);
+			todoList.get(selectedIndex).put("check", check);
+			todoList.get(selectedIndex).put("addr", todo);
+			saveToFile();
+			JOptionPane.showMessageDialog(this, "수정했습니다.");			
 		}else if(e.getSource() == deleteBtn){
-			
+
+			int selectedIndex=table.getSelectedRow();
+			if(selectedIndex==-1){
+				JOptionPane.showMessageDialog
+					(this, "삭제할 row 를 선택하세요");
+				return; 
+			}
+
+			todoList.remove(selectedIndex);
+			saveToFile();
+			JOptionPane.showMessageDialog(this, "삭제했습니다.");			
 		}
+		loadFromFile();
+		displayMember();
 	}
 	
-	public void saveToFile(){
-		//파일에 출력해서 영구 저장한다.		
+	public void saveToFile(){		
 		FileOutputStream fos=null;
 		ObjectOutputStream oos=null;
 		try{
 			fos=new FileOutputStream("c:/myFolder/MyTodo.dat");
-			//객체 정보를 출력할수 있는 객체 생성하기 
 			oos=new ObjectOutputStream(fos);
-			//List 객체 출력하기 
 			oos.writeObject(todoList);
 			oos.flush();
 		}catch(Exception e){
@@ -142,6 +183,45 @@ public class testProgram extends JFrame implements ActionListener{
 			}catch(Exception e){}
 		}
 	}
+	public void loadFromFile(){
+		FileInputStream fis=null;
+		ObjectInputStream ois=null;
+		try{
+			fis=new FileInputStream("c:/myFolder/MyTodo.dat");
+			ois=new ObjectInputStream(fis); 
+			todoList=(List)ois.readObject();
+		}catch(FileNotFoundException fne){
+			todoList=new ArrayList<>();
+			System.out.println("todoList.dat 파일이 없음");
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally{
+			try{
+				if(fis!=null)fis.close();
+				if(ois!=null)ois.close();
+			}catch(Exception e){}
+		}
+	}
+	
+	//맴버필드에 있는 회원정보를 출력하는 메소드
+	public void displayMember(){
+		//table 에 출력된 내용 삭제 되도록
+		model.setRowCount(0);
+		
+		for(Map<String, Object> tmp:todoList){
+			String check=(String)tmp.get("Check");
+			String month=(String)tmp.get("Month");
+			String date=(String)tmp.get("Date");
+			String todo=(String)tmp.get("ToDo");
+			Object[] row=new Object[3];
+			row[0]=check;
+			row[1]=month+"월 "+date+"일";
+			row[2]=todo;
+			model.addRow(row);
+		}
+	}
+		
+	
 	public static void main(String[] args) {
 		new testProgram();
 	}
---
+++ Step23_Swing/src/example3/CalcLayout.java
@@ -0,0 +1,23 @@
+package example3;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+
+public class CalcLayout extends JFrame implements ActionListener{
+	
+	public CalcLayout(){
+		
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		
+	}
+}
